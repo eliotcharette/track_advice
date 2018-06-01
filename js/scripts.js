a3c04591957e5eaa2e62ead54a8bd02dff1c5631
Guest User <Guest@epicodus-12.local>
2018-06-01 11:35:15 -0700

    Add styles to survey questions and results

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,27 @@
 $(function(){
   $("#formOne").submit(function(event) {
 
-    var jobs = parseInt($("input:radio[name=marriage]:checked").val());
-    var difficult = parseInt($("input:radio[name=healthcare]:checked").val());
-    var speed = parseInt($("input:radio[name=breitbart]:checked").val());
-    var popular = parseInt($("input:radio[name=marriage]:checked").val());
-    var tools = parseInt($("input:radio[name=healthcare]:checked").val());
-    var fundamentals = parseInt($("input:radio[name=breitbart]:checked").val());
+    var jobs = parseInt($("input:radio[name=jobs]:checked").val());
+    var difficult = parseInt($("input:radio[name=difficult]:checked").val());
+    var speed = parseInt($("input:radio[name=speed]:checked").val());
+    var popular = parseInt($("input:radio[name=popular]:checked").val());
+    var tools = parseInt($("input:radio[name=tools]:checked").val());
+    var fundamentals = parseInt($("input:radio[name=fundamentals]:checked").val());
 
     var add = jobs + difficult + speed + popular + tools + fundamentals;
 
-      if (add > 30) {
+      if (add > 39) {
         $('#js').show();
         $('#csharp').hide();
         $('#ruby').hide();
-      } else if (add > 15 < 30){
-        $('#csharp').show();
-        $('#js').hide();
-        $('#ruby').hide();
-      } else {
+      } else if (add < 16){
         $('#ruby').show();
         $('#js').hide();
         $('#csharp').hide();
+      } else {
+        $('#csharp').show();
+        $('#js').hide();
+        $('#ruby').hide();
       }
 
 
