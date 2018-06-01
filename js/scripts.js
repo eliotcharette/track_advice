99aef149c9a7b883e2e74ac8d9e34420b3504906
Guest User <Guest@epicodus-12.local>
2018-06-01 11:00:26 -0700

    Add variables to scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,27 @@
 $(function(){
   $("#formOne").submit(function(event) {
 
+    var jobs = parseInt($("input:radio[name=marriage]:checked").val());
+    var difficult = parseInt($("input:radio[name=healthcare]:checked").val());
+    var speed = parseInt($("input:radio[name=breitbart]:checked").val());
+    var popular = parseInt($("input:radio[name=marriage]:checked").val());
+    var tools = parseInt($("input:radio[name=healthcare]:checked").val());
+    var fundamentals = parseInt($("input:radio[name=breitbart]:checked").val());
 
-    var marriage = parseInt($("input:radio[name=marriage]:checked").val());
+    var add = jobs + difficult + speed + popular + tools + fundamentals;
 
-    var healthcare = parseInt($("input:radio[name=healthcare]:checked").val());
-
-    var breitbart = parseInt($("input:radio[name=breitbart]:checked").val());
-
-    var add = marriage + healthcare + breitbart;
-
-      if (add > 11) {
+      if (add > 30) {
         $('#js').show();
-        $('#liberal').hide();
-      } else if{
-        $('#liberal').show();
-        $('#conservative').hide();
+        $('#csharp').hide();
+        $('#ruby').hide();
+      } else if (add > 15 < 30){
+        $('#csharp').show();
+        $('#js').hide();
+        $('#ruby').hide();
+      } else {
+        $('#ruby').show();
+        $('#js').hide();
+        $('#csharp').hide();
       }
 
 
